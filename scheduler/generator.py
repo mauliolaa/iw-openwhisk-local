ba7c8a82c311dd36e232a2173ba363b7de7bc383
JothamWong <45916998+JothamWong@users.noreply.github.com>
2023-11-15 04:01:44 -0500

    Add simple workload generator

--- scheduler/generator.py
+++ scheduler/generator.py
@@ -1,3 +1,28 @@
 """
 Generate a workload file.
 """
+import sys
+
+import numpy as np
+
+def usage():
+    print("python generator.py {filename} {n} {mean} {variance} {num_functions}")
+    print("ideally, mean and variance should be such that sampled durations are not negative")
+    exit(0)
+    
+
+if __name__ == "__main__":
+    if len(sys.argv) != 6:
+        usage()
+    filename = sys.argv[1]
+    n = int(sys.argv[2])
+    mean = float(sys.argv[3])
+    var = float(sys.argv[4])
+    num_funcs = int(sys.argv[5])
+    timestamps = np.random.normal(mean, var, n)
+    timestamps = np.cumsum(timestamps)
+    timestamps = np.round(timestamps)
+    functions = np.random.randint(low=0, high=num_funcs, size=n)
+    with open(filename, "w") as outf:
+        for timestamp, func in zip(timestamps, functions):
+            outf.write(f"{str(timestamp)}: {func}\n")
