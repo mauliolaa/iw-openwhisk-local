5bb57f7af1a3683c195724ae7b7841f556d898f2
Chai Mauliola <mauliola@princeton.edu>
2023-11-23 18:31:22 -0500

    time function invocations

--- scheduler/generator.py
+++ scheduler/generator.py
@@ -19,10 +19,22 @@ if __name__ == "__main__":
     mean = float(sys.argv[3])
     var = float(sys.argv[4])
     num_funcs = int(sys.argv[5])
+
+    # Note, what units are these timestamps in?
     timestamps = np.random.normal(mean, var, n)
     timestamps = np.cumsum(timestamps)
     timestamps = np.round(timestamps)
     functions = np.random.randint(low=0, high=num_funcs, size=n)
     with open(filename, "w") as outf:
         for timestamp, func in zip(timestamps, functions):
-            outf.write(f"{str(timestamp)}: {func}\n")
+            # Note: I think that our workload files should be in CSV format, since CSV
+            # is easy to read and a bit less of a hassle if we want to start adding 
+            # more parameters to functions, etcetera.
+
+            # If we are going to store function number, then we will also need to have a
+            # map associating faas functions with thier number. For example,
+            # 0 = '/myfunction-1', 1 = '/otherfunction', 2 = '/finalfunction' 
+
+            # Format will be something like this:
+            # timestamp, function_number
+            outf.write(f"{str(timestamp)},{func}\n")
---
+++ scheduler/workload_format.py
@@ -0,0 +1,5 @@
+# Dictionary mapping CSV indexes to named workload parameters.
+csv_params = {
+    "timestamp": 0,
+    "function_number": 1
+}
