b47f3bdc6ddeb0c061e995f9fa295cc6c3e488bd
JothamWong <45916998+JothamWong@users.noreply.github.com>
2023-12-01 21:46:47 -0500

    Switch requests to async

--- slavemaster/simulate.py
+++ slavemaster/simulate.py
@@ -1,6 +1,8 @@
 """Read in a workload file and send requests in specified order"""
 import sys
 import requests
+import asyncio
+import aiohttp  # Used to make asynchronous http requests because we don't actually want to wait for the http request to be done
 import time
 import subprocess
 
@@ -18,8 +20,8 @@ def usage():
     """
     print(usage)
     exit()
-
-if __name__ == "__main__":
+    
+async def main():
     if len(sys.argv) != 4:
         usage()
     workload_filename = sys.argv[1]
@@ -51,40 +53,40 @@ if __name__ == "__main__":
             result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             if result.returncode != 0:
                 print(f"Error: {result.stderr}")
-                exit(1)    
+                exit(1)
 
     start_time = time.time()
-    with open(workload_filename, "r") as inf:
-        lines = inf.read().splitlines()
-        for i, line in enumerate(lines):
-            # Format is [time_delta],[action],[params...]
-            # Should split into timestamp, function name, param
-            print(f"Executing line {i}: {line}")
-            p = line.split(sep=',')
-            delta = float(p[0])
-            functionName = p[1]
-            
-            params = {}
-            for faas_param in p[2:]:
-                param_name, param_value = faas_param.split(":")
-                params[param_name] = param_value
-            time.sleep(max(0, delta))
-            query_url = faas_url + "/receive?fnName=" + functionName
-            
-            for param_name, param_value in params.items():
-                query_url += "&" + param_name + "=" + param_value
+    async with aiohttp.ClientSession() as session:
+        with open(workload_filename, "r") as inf:
+            lines = inf.read().splitlines()
+            for i, line in enumerate(lines):
+                # Format is [time_delta],[action],[params...]
+                # Should split into timestamp, function name, param
+                print(f"Executing line {i}: {line}")
+                p = line.split(sep=',')
+                delta = float(p[0])
+                functionName = p[1]
                 
-            print(f"Query url is {query_url}") 
-            r = requests.get(query_url)
-            print(r)
-            if r.status_code != 200:
-                raise RuntimeError(f"Function returned non-200 status code.\nURL: {r.url}\nCode: {r.status_code}")
-            
-            # Time between request send and response in miliseconds.
-            elapsed_time = round(r.elapsed.total_seconds() * 1000)
-            print("Elapsed time for request {}:\n{} ms\n".format(r.url, elapsed_time))
+                params = {}
+                for faas_param in p[2:]:
+                    param_name, param_value = faas_param.split(":")
+                    params[param_name] = param_value
+                print(f"Sleeping for {delta} seconds")
+                time.sleep(max(0, delta))
+                query_url = faas_url + "/receive?fnName=" + functionName
+                
+                for param_name, param_value in params.items():
+                    query_url += "&" + param_name + "=" + param_value
+                    
+                print(f"Query url is {query_url}") 
+                async with session.get(query_url) as r:
+                    _ = await r.json(content_type=None)  # disable decoding of json cos we don't really care
+                    # if stuff.status_code != 200:
+                    #     raise RuntimeError(f"Function returned non-200 status code.\nURL: {r.url}\nCode: {r.status_code}")
 
     # Now, graph elapsed time data
-    elasped_time = time.time() - start_time
+    elapsed_time = time.time() - start_time
     print(f"Simulation elapsed time: %s", elapsed_time)
-            
+
+if __name__ == "__main__":
+    asyncio.run(main())
