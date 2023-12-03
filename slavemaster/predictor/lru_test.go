1435f00f9a96f53459a96e56c0bbcf9d8024c5cc
JothamWong <45916998+JothamWong@users.noreply.github.com>
2023-12-03 02:33:13 -0500

    Add lru predictor test

--- slavemaster/predictor/lru_test.go
+++ slavemaster/predictor/lru_test.go
@@ -1,1 +1,51 @@
 package predictor
+
+import (
+	"container/list"
+	"fmt"
+	"testing"
+)
+
+func TestLRU(t *testing.T) {
+	// size of 2
+	lru := NewLRU("../lru_config.yaml")
+	// test that the evict works as expected
+	info1 := make(map[string]any)
+	info1["fnRequest"] = FnRequest{
+		FnName: "a",
+		FnParameters: map[string]string{
+			"p1": "1",
+		},
+	}
+	info2 := make(map[string]any)
+	info2["fnRequest"] = FnRequest{
+		FnName: "b",
+		FnParameters: map[string]string{
+			"p1": "2",
+		},
+	}
+	info3 := make(map[string]any)
+	info3["fnRequest"] = FnRequest{
+		FnName: "c",
+		FnParameters: map[string]string{
+			"p1": "3",
+		},
+	}
+	lru.Update(info1)
+	lru.Update(info2)
+	lru.Update(info3)
+	// we expect that lru should only have functions a and c, cos b is MRU and should be ignored
+	ll := lru.lst
+	var e *list.Element
+	for e = ll.Front(); e != nil; e = e.Next() {
+		fmt.Println("element: ", e.Value)
+		if e.Value == "b" {
+			t.Error("Expect c to not be in lru but encountered it")
+		}
+	}
+	// now we check that lru should predict fn a
+	prediction := lru.Predict()
+	if prediction.FnName != "a" {
+		t.Errorf("Expected fn prediction to be a but it was %s", prediction.FnName)
+	}
+}
