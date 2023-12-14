5cae8254ea5b14142c5579a8a88b1c11786d3731
JothamWong <45916998+JothamWong@users.noreply.github.com>
2023-12-14 18:42:54 -0500

    Add scripts for scraping and plotting data
    Add script to plot the container states for each strategy and periodicity in a stacked bar graph.
    
    Also add a script to obtain the average latency for each language runtime.

---
+++ plot_container_states.py
@@ -0,0 +1,99 @@
+import sys
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+if len(sys.argv) != 1:
+    usage = '''[Usage]: python3 plot_results.py 
+    '''
+    print(usage)
+    exit(1)
+
+strats = [
+    "lru",
+    "mru",
+    "mfe",
+    "pq",
+    # "rs",
+    "naive"
+]
+
+periods = [5,]
+# periods = ["5", "10", "15"]
+
+total_cold_container_stats = {
+    5: {},
+    10: {},
+    15: {},
+}
+
+total_warm_container_stats = {
+    5: {},
+    10: {},
+    15: {},
+}
+
+total_prewarmed_container_stats = {
+    5: {},
+    10: {},
+    15: {},
+}
+
+total_recreated_container_stats = {
+    5: {},
+    10: {},
+    15: {},
+}
+
+for strat in strats:
+    for period in periods:
+        if strat == "naive":
+            prefix = "naive"
+        else:
+            prefix = f"{strat}_{period}"
+        with open(f"./{prefix}/results.json", "r") as f:
+            json_data = json.load(f)
+        for fun_name in json_data.keys():
+            if fun_name == "languages":
+                continue
+            if strat in total_cold_container_stats[period]:
+                total_cold_container_stats[period][strat] += json_data[fun_name]["coldContainerCount"]
+            else:
+                total_cold_container_stats[period][strat] = 0
+
+            if strat in total_warm_container_stats[period]:
+                total_warm_container_stats[period][strat] += json_data[fun_name]["warmedContainerCount"]
+            else:
+                total_warm_container_stats[period][strat] = 0
+
+            if strat in total_prewarmed_container_stats[period]:
+                total_prewarmed_container_stats[period][strat] += json_data[fun_name]["prewarmedContainerCount"]
+            else:
+                total_prewarmed_container_stats[period][strat] = 0
+                
+            if strat in total_recreated_container_stats[period]:
+                total_recreated_container_stats[period][strat] += json_data[fun_name]["recreatedContainerCount"]
+            else:
+                total_recreated_container_stats[period][strat] = 0
+
+pprint(total_cold_container_stats)
+
+    
+for period in periods:
+    period = int(period)
+    fig_title = f"Period {period}"
+    fig, axs = plt.subplots(1, 1)
+    bottom = np.zeros(len(strats))
+    print(total_cold_container_stats.keys())
+    print(total_cold_container_stats[period].values())
+    axs.bar(total_cold_container_stats[period].keys(), total_cold_container_stats[period].values(), label="Cold", color="blue")
+    axs.bar(total_warm_container_stats[period].keys(), total_warm_container_stats[period].values(), bottom=np.array(list(total_cold_container_stats[period].values())), label="Warmed", color="red")
+    axs.bar(total_prewarmed_container_stats[period].keys(), total_prewarmed_container_stats[period].values(), bottom=np.array(list(total_cold_container_stats[period].values())) + np.array(list(total_warm_container_stats[period].values())),label="Prewarmed", color="salmon")
+    axs.bar(total_recreated_container_stats[period].keys(), total_recreated_container_stats[period].values(), label="Recreated", bottom=np.array(list(total_cold_container_stats[period].values())) + np.array(list(total_warm_container_stats[period].values())) + np.array(list(total_prewarmed_container_stats[period].values())), color="dodgerblue")
+    plt.title("Total number of containers by strategy")
+    plt.xlabel("Strategy")
+    plt.ylabel("Number of container starts")
+    plt.legend()
+    plt.savefig(fig_title + ".png", bbox_inches="tight")
+    plt.clf()
---
+++ plot_latencies.py
@@ -0,0 +1,49 @@
+"""This script obtains the average latencies for each language for each periodicity and strategy"""
+import json
+
+periods = [5, 10, 15]
+strats = [
+    "lru",
+    # "mfe",
+    "mru",
+    "pq",
+    "naive",
+]
+# {action: {period: | {language
+# Results table
+# strategies periodicity cold warm prewarmed recreated py java rb php js
+results = {
+    strat: {period: {} for period in periods} for strat in strats
+}
+
+print(results)
+
+action_to_language_mapping = {}
+
+with open("taskmaster/functions_test", "r") as inf:
+    for line in inf.readlines():
+        line = line.strip()
+        action = line.split(" ")[0]
+        language = line.split(" ")[1].split(".")[1]
+        action_to_language_mapping[action] = language
+
+for strat in strats:
+    for period in periods:
+        if strat == "naive":
+            filename = f"{strat}/results.json"
+        else:
+            filename = f"{strat}_{period}/results.json"
+        with open(filename, "r") as f:
+            sub_data = json.load(f)
+            for action, language in action_to_language_mapping.items():
+                timings = sub_data[action]["elapsedTimes"]
+                if language not in results[strat][period]:  # First time
+                    results[strat][period][language] = timings
+                else:  # All files accounted for
+                    results[strat][period][language].extend(timings)
+                    elapsed_timings = results[strat][period][language]
+                    results[strat][period][language] = sum(elapsed_timings) / len(elapsed_timings)
+
+print(results)
+with open("latencies.json", "w") as outf:
+    json.dump(results, outf)
